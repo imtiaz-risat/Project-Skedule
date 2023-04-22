5f094f52fc975e691888eb0038b34e7f7d297b37
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2023-04-23 00:55:35 +0600

    Dashboard Child Form Added and Updated

---
+++ DashboardChildForm.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Skedule
+{
+    partial class DashboardChildForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardChildForm));
+            this.label_MyList = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelClock = new System.Windows.Forms.Label();
+            this.label_upcomingquiz = new System.Windows.Forms.Label();
+            this.label_pendingtasks = new System.Windows.Forms.Label();
+            this.label_daysbefore = new System.Windows.Forms.Label();
+            this.label_FINAL = new System.Windows.Forms.Label();
+            this.labelNumUpcomingQuiz = new System.Windows.Forms.Label();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelNumPendingTasks = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelNumDayFinal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelDashboardUp = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelDashboardUp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_MyList
+            // 
+            this.label_MyList.AutoSize = true;
+            this.label_MyList.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_MyList.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_MyList.Location = new System.Drawing.Point(17, 203);
+            this.label_MyList.Name = "label_MyList";
+            this.label_MyList.Size = new System.Drawing.Size(96, 32);
+            this.label_MyList.TabIndex = 11;
+            this.label_MyList.Text = "My List";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.BackColor = System.Drawing.Color.White;
+            this.labelDate.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.ForeColor = System.Drawing.Color.Black;
+            this.labelDate.Location = new System.Drawing.Point(345, 72);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(191, 21);
+            this.labelDate.TabIndex = 1;
+            this.labelDate.Text = "Thursday, 21 April, 2023";
+            this.labelDate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelClock
+            // 
+            this.labelClock.AutoSize = true;
+            this.labelClock.BackColor = System.Drawing.Color.White;
+            this.labelClock.Font = new System.Drawing.Font("Ebrima", 34F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClock.ForeColor = System.Drawing.Color.Black;
+            this.labelClock.Location = new System.Drawing.Point(324, 21);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(228, 62);
+            this.labelClock.TabIndex = 1;
+            this.labelClock.Text = "12:00 PM";
+            // 
+            // label_upcomingquiz
+            // 
+            this.label_upcomingquiz.AutoSize = true;
+            this.label_upcomingquiz.Font = new System.Drawing.Font("Ebrima", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_upcomingquiz.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_upcomingquiz.Location = new System.Drawing.Point(725, 138);
+            this.label_upcomingquiz.Name = "label_upcomingquiz";
+            this.label_upcomingquiz.Size = new System.Drawing.Size(129, 19);
+            this.label_upcomingquiz.TabIndex = 1;
+            this.label_upcomingquiz.Text = "upcoming quizzes";
+            // 
+            // label_pendingtasks
+            // 
+            this.label_pendingtasks.AutoSize = true;
+            this.label_pendingtasks.Font = new System.Drawing.Font("Ebrima", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_pendingtasks.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_pendingtasks.Location = new System.Drawing.Point(613, 138);
+            this.label_pendingtasks.Name = "label_pendingtasks";
+            this.label_pendingtasks.Size = new System.Drawing.Size(101, 19);
+            this.label_pendingtasks.TabIndex = 1;
+            this.label_pendingtasks.Text = "pending tasks";
+            // 
+            // label_daysbefore
+            // 
+            this.label_daysbefore.AutoSize = true;
+            this.label_daysbefore.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_daysbefore.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_daysbefore.Location = new System.Drawing.Point(712, 21);
+            this.label_daysbefore.Name = "label_daysbefore";
+            this.label_daysbefore.Size = new System.Drawing.Size(99, 21);
+            this.label_daysbefore.TabIndex = 1;
+            this.label_daysbefore.Text = "days before";
+            // 
+            // label_FINAL
+            // 
+            this.label_FINAL.AutoSize = true;
+            this.label_FINAL.Font = new System.Drawing.Font("Ebrima", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_FINAL.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_FINAL.Location = new System.Drawing.Point(708, 33);
+            this.label_FINAL.Name = "label_FINAL";
+            this.label_FINAL.Size = new System.Drawing.Size(106, 42);
+            this.label_FINAL.TabIndex = 1;
+            this.label_FINAL.Text = "FINAL";
+            // 
+            // labelNumUpcomingQuiz
+            // 
+            this.labelNumUpcomingQuiz.AutoSize = true;
+            this.labelNumUpcomingQuiz.Font = new System.Drawing.Font("Ebrima", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumUpcomingQuiz.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelNumUpcomingQuiz.Location = new System.Drawing.Point(754, 91);
+            this.labelNumUpcomingQuiz.Name = "labelNumUpcomingQuiz";
+            this.labelNumUpcomingQuiz.Size = new System.Drawing.Size(69, 54);
+            this.labelNumUpcomingQuiz.TabIndex = 1;
+            this.labelNumUpcomingQuiz.Text = "02";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Task Details";
+            this.columnHeader3.Width = 460;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "End Date";
+            this.columnHeader2.Width = 130;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Start Date";
+            this.columnHeader1.Width = 130;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(23, 237);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(826, 266);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Completed";
+            this.columnHeader4.Width = 100;
+            // 
+            // labelNumPendingTasks
+            // 
+            this.labelNumPendingTasks.AutoSize = true;
+            this.labelNumPendingTasks.Font = new System.Drawing.Font("Ebrima", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumPendingTasks.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelNumPendingTasks.Location = new System.Drawing.Point(629, 91);
+            this.labelNumPendingTasks.Name = "labelNumPendingTasks";
+            this.labelNumPendingTasks.Size = new System.Drawing.Size(69, 54);
+            this.labelNumPendingTasks.TabIndex = 1;
+            this.labelNumPendingTasks.Text = "08";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Ebrima", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button3.Image = global::Skedule.Properties.Resources.add_green_32x32;
+            this.button3.Location = new System.Drawing.Point(729, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 41);
+            this.button3.TabIndex = 7;
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Ebrima", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button2.Image = global::Skedule.Properties.Resources.edit_yellow_32x32;
+            this.button2.Location = new System.Drawing.Point(773, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 41);
+            this.button2.TabIndex = 8;
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // labelNumDayFinal
+            // 
+            this.labelNumDayFinal.AutoSize = true;
+            this.labelNumDayFinal.Font = new System.Drawing.Font("Ebrima", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumDayFinal.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelNumDayFinal.Location = new System.Drawing.Point(641, 12);
+            this.labelNumDayFinal.Name = "labelNumDayFinal";
+            this.labelNumDayFinal.Size = new System.Drawing.Size(84, 65);
+            this.labelNumDayFinal.TabIndex = 1;
+            this.labelNumDayFinal.Text = "72";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Ebrima", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button1.Image = global::Skedule.Properties.Resources.delete_red_32x32;
+            this.button1.Location = new System.Drawing.Point(814, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 41);
+            this.button1.TabIndex = 9;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panelDashboardUp
+            // 
+            this.panelDashboardUp.Controls.Add(this.labelDate);
+            this.panelDashboardUp.Controls.Add(this.labelClock);
+            this.panelDashboardUp.Controls.Add(this.label_upcomingquiz);
+            this.panelDashboardUp.Controls.Add(this.label_pendingtasks);
+            this.panelDashboardUp.Controls.Add(this.label_daysbefore);
+            this.panelDashboardUp.Controls.Add(this.pictureBox1);
+            this.panelDashboardUp.Controls.Add(this.label_FINAL);
+            this.panelDashboardUp.Controls.Add(this.labelNumUpcomingQuiz);
+            this.panelDashboardUp.Controls.Add(this.labelNumPendingTasks);
+            this.panelDashboardUp.Controls.Add(this.labelNumDayFinal);
+            this.panelDashboardUp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDashboardUp.Location = new System.Drawing.Point(0, 0);
+            this.panelDashboardUp.Name = "panelDashboardUp";
+            this.panelDashboardUp.Size = new System.Drawing.Size(869, 173);
+            this.panelDashboardUp.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(566, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DashboardChildForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.Controls.Add(this.label_MyList);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panelDashboardUp);
+            this.Name = "DashboardChildForm";
+            this.Size = new System.Drawing.Size(869, 516);
+            this.panelDashboardUp.ResumeLayout(false);
+            this.panelDashboardUp.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_MyList;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelClock;
+        private System.Windows.Forms.Label label_upcomingquiz;
+        private System.Windows.Forms.Label label_pendingtasks;
+        private System.Windows.Forms.Label label_daysbefore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_FINAL;
+        private System.Windows.Forms.Label labelNumUpcomingQuiz;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label labelNumPendingTasks;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelNumDayFinal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelDashboardUp;
+    }
+}
