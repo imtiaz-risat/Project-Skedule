e513cd20dce15e32a16a9702216f2805fae574e5
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2023-04-30 22:53:35 +0600

    Classroom Class added and updated

---
+++ Classroom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skedule
+{
+    internal class Classroom
+    {
+        public string classCode { get; set; }
+        public Routine routineSet { get; set; }
+        public List<Subject> subjectList { get; set; } = new List<Subject>();
+        public List<Exam> examList { get; set; } = new List<Exam>();
+        public List<Task> taskList { get; set; } = new List<Task>();
+        public List<Annoucement> annoucementList { get; set;} = new List<Annoucement>();
+
+        // Discussion Section is yet to be added
+    }
+}
