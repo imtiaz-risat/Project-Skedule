b5240966c8d45d5db7388477ef1a953fdcffcec3
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2023-04-30 22:45:46 +0600

    Task Class added

---
+++ Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skedule
+{
+    internal class Task
+    {
+        public Subject Subject { get; set; }
+        public string taskTag { get; set; }
+        public string taskDetails { get; set; }
+        public DateTime deadline { get; set; }
+    }
+}
