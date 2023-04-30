33014a7af1500fe84729a26cc2a7584fad7cd206
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2023-04-30 22:38:43 +0600

    Subject Class added

---
+++ Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skedule
+{
+    internal class Subject
+    {
+        public string subjectCode {  get; set; }
+        public string subjectName { get; set; }
+        
+    }
+}
