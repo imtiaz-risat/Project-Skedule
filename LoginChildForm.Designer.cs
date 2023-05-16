fd7d63747ec4e23868110a15f2c091f4e022f1f7
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2023-05-17 00:14:01 +0600

    Login and Signup functions added

--- LoginChildForm.Designer.cs
+++ LoginChildForm.Designer.cs
@@ -44,6 +44,12 @@
             this.labelSigupEmail = new System.Windows.Forms.Label();
             this.labelLoginUsername = new System.Windows.Forms.Label();
             this.labelLogin = new System.Windows.Forms.Label();
+            this.textBoxSignupFirstname = new System.Windows.Forms.TextBox();
+            this.textBoxSignupLastname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.birthdatePickerSignup = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // buttonSignup
@@ -54,12 +60,13 @@
             this.buttonSignup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSignup.Font = new System.Drawing.Font("Ebrima", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSignup.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonSignup.Location = new System.Drawing.Point(156, 368);
+            this.buttonSignup.Location = new System.Drawing.Point(156, 405);
             this.buttonSignup.Name = "buttonSignup";
             this.buttonSignup.Size = new System.Drawing.Size(162, 45);
             this.buttonSignup.TabIndex = 17;
             this.buttonSignup.Text = "Signup";
             this.buttonSignup.UseVisualStyleBackColor = false;
+            this.buttonSignup.Click += new System.EventHandler(this.buttonSignup_Click);
             // 
             // buttonLogin
             // 
@@ -69,17 +76,18 @@
             this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonLogin.Font = new System.Drawing.Font("Ebrima", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonLogin.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonLogin.Location = new System.Drawing.Point(555, 271);
+            this.buttonLogin.Location = new System.Drawing.Point(555, 295);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(162, 45);
             this.buttonLogin.TabIndex = 18;
             this.buttonLogin.Text = "Login";
             this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
             // textBoxSignupConfirmPass
             // 
             this.textBoxSignupConfirmPass.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSignupConfirmPass.Location = new System.Drawing.Point(130, 322);
+            this.textBoxSignupConfirmPass.Location = new System.Drawing.Point(130, 266);
             this.textBoxSignupConfirmPass.Name = "textBoxSignupConfirmPass";
             this.textBoxSignupConfirmPass.Size = new System.Drawing.Size(214, 28);
             this.textBoxSignupConfirmPass.TabIndex = 11;
@@ -87,7 +95,7 @@
             // textBoxSignupPassword
             // 
             this.textBoxSignupPassword.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSignupPassword.Location = new System.Drawing.Point(130, 271);
+            this.textBoxSignupPassword.Location = new System.Drawing.Point(130, 215);
             this.textBoxSignupPassword.Name = "textBoxSignupPassword";
             this.textBoxSignupPassword.Size = new System.Drawing.Size(214, 28);
             this.textBoxSignupPassword.TabIndex = 12;
@@ -95,7 +103,7 @@
             // textBoxSignupUsername
             // 
             this.textBoxSignupUsername.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSignupUsername.Location = new System.Drawing.Point(130, 220);
+            this.textBoxSignupUsername.Location = new System.Drawing.Point(130, 164);
             this.textBoxSignupUsername.Name = "textBoxSignupUsername";
             this.textBoxSignupUsername.Size = new System.Drawing.Size(214, 28);
             this.textBoxSignupUsername.TabIndex = 13;
@@ -103,7 +111,7 @@
             // textBoxSignupEmail
             // 
             this.textBoxSignupEmail.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSignupEmail.Location = new System.Drawing.Point(130, 169);
+            this.textBoxSignupEmail.Location = new System.Drawing.Point(130, 113);
             this.textBoxSignupEmail.Name = "textBoxSignupEmail";
             this.textBoxSignupEmail.Size = new System.Drawing.Size(214, 28);
             this.textBoxSignupEmail.TabIndex = 14;
@@ -111,7 +119,7 @@
             // textBoxLoginPassword
             // 
             this.textBoxLoginPassword.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxLoginPassword.Location = new System.Drawing.Point(531, 220);
+            this.textBoxLoginPassword.Location = new System.Drawing.Point(531, 244);
             this.textBoxLoginPassword.Name = "textBoxLoginPassword";
             this.textBoxLoginPassword.Size = new System.Drawing.Size(214, 28);
             this.textBoxLoginPassword.TabIndex = 15;
@@ -119,7 +127,7 @@
             // textBoxLoginUsername
             // 
             this.textBoxLoginUsername.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxLoginUsername.Location = new System.Drawing.Point(531, 169);
+            this.textBoxLoginUsername.Location = new System.Drawing.Point(531, 193);
             this.textBoxLoginUsername.Name = "textBoxLoginUsername";
             this.textBoxLoginUsername.Size = new System.Drawing.Size(214, 28);
             this.textBoxLoginUsername.TabIndex = 16;
@@ -129,7 +137,7 @@
             this.labelSignup.AutoSize = true;
             this.labelSignup.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSignup.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelSignup.Location = new System.Drawing.Point(124, 104);
+            this.labelSignup.Location = new System.Drawing.Point(124, 48);
             this.labelSignup.Name = "labelSignup";
             this.labelSignup.Size = new System.Drawing.Size(94, 32);
             this.labelSignup.TabIndex = 3;
@@ -140,7 +148,7 @@
             this.labelSignupConfirmPass.AutoSize = true;
             this.labelSignupConfirmPass.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSignupConfirmPass.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelSignupConfirmPass.Location = new System.Drawing.Point(126, 301);
+            this.labelSignupConfirmPass.Location = new System.Drawing.Point(126, 245);
             this.labelSignupConfirmPass.Name = "labelSignupConfirmPass";
             this.labelSignupConfirmPass.Size = new System.Drawing.Size(137, 20);
             this.labelSignupConfirmPass.TabIndex = 4;
@@ -151,7 +159,7 @@
             this.labelLoginPassword.AutoSize = true;
             this.labelLoginPassword.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLoginPassword.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelLoginPassword.Location = new System.Drawing.Point(527, 199);
+            this.labelLoginPassword.Location = new System.Drawing.Point(527, 223);
             this.labelLoginPassword.Name = "labelLoginPassword";
             this.labelLoginPassword.Size = new System.Drawing.Size(76, 20);
             this.labelLoginPassword.TabIndex = 5;
@@ -162,7 +170,7 @@
             this.labelSignupPassword.AutoSize = true;
             this.labelSignupPassword.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSignupPassword.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelSignupPassword.Location = new System.Drawing.Point(126, 250);
+            this.labelSignupPassword.Location = new System.Drawing.Point(126, 194);
             this.labelSignupPassword.Name = "labelSignupPassword";
             this.labelSignupPassword.Size = new System.Drawing.Size(76, 20);
             this.labelSignupPassword.TabIndex = 6;
@@ -173,7 +181,7 @@
             this.labelSignupUsername.AutoSize = true;
             this.labelSignupUsername.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSignupUsername.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelSignupUsername.Location = new System.Drawing.Point(126, 199);
+            this.labelSignupUsername.Location = new System.Drawing.Point(126, 143);
             this.labelSignupUsername.Name = "labelSignupUsername";
             this.labelSignupUsername.Size = new System.Drawing.Size(80, 20);
             this.labelSignupUsername.TabIndex = 7;
@@ -184,7 +192,7 @@
             this.labelSigupEmail.AutoSize = true;
             this.labelSigupEmail.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSigupEmail.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelSigupEmail.Location = new System.Drawing.Point(126, 148);
+            this.labelSigupEmail.Location = new System.Drawing.Point(126, 92);
             this.labelSigupEmail.Name = "labelSigupEmail";
             this.labelSigupEmail.Size = new System.Drawing.Size(47, 20);
             this.labelSigupEmail.TabIndex = 8;
@@ -195,7 +203,7 @@
             this.labelLoginUsername.AutoSize = true;
             this.labelLoginUsername.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLoginUsername.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelLoginUsername.Location = new System.Drawing.Point(527, 146);
+            this.labelLoginUsername.Location = new System.Drawing.Point(527, 170);
             this.labelLoginUsername.Name = "labelLoginUsername";
             this.labelLoginUsername.Size = new System.Drawing.Size(80, 20);
             this.labelLoginUsername.TabIndex = 9;
@@ -206,19 +214,78 @@
             this.labelLogin.AutoSize = true;
             this.labelLogin.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLogin.ForeColor = System.Drawing.SystemColors.Control;
-            this.labelLogin.Location = new System.Drawing.Point(525, 104);
+            this.labelLogin.Location = new System.Drawing.Point(525, 128);
             this.labelLogin.Name = "labelLogin";
             this.labelLogin.Size = new System.Drawing.Size(78, 32);
             this.labelLogin.TabIndex = 10;
             this.labelLogin.Text = "Login";
+            // 
+            // textBoxSignupFirstname
+            // 
+            this.textBoxSignupFirstname.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSignupFirstname.Location = new System.Drawing.Point(130, 318);
+            this.textBoxSignupFirstname.Name = "textBoxSignupFirstname";
+            this.textBoxSignupFirstname.Size = new System.Drawing.Size(104, 28);
+            this.textBoxSignupFirstname.TabIndex = 11;
+            // 
+            // textBoxSignupLastname
+            // 
+            this.textBoxSignupLastname.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSignupLastname.Location = new System.Drawing.Point(240, 318);
+            this.textBoxSignupLastname.Name = "textBoxSignupLastname";
+            this.textBoxSignupLastname.Size = new System.Drawing.Size(104, 28);
+            this.textBoxSignupLastname.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(126, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Firstname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(236, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ebrima", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(127, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date of Birth";
+            // 
+            // birthdatePickerSignup
+            // 
+            this.birthdatePickerSignup.Location = new System.Drawing.Point(131, 372);
+            this.birthdatePickerSignup.Name = "birthdatePickerSignup";
+            this.birthdatePickerSignup.Size = new System.Drawing.Size(213, 20);
+            this.birthdatePickerSignup.TabIndex = 19;
             // 
             // LoginChildForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.Controls.Add(this.birthdatePickerSignup);
             this.Controls.Add(this.buttonSignup);
             this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxSignupLastname);
+            this.Controls.Add(this.textBoxSignupFirstname);
             this.Controls.Add(this.textBoxSignupConfirmPass);
             this.Controls.Add(this.textBoxSignupPassword);
             this.Controls.Add(this.textBoxSignupUsername);
@@ -226,6 +293,9 @@
             this.Controls.Add(this.textBoxLoginPassword);
             this.Controls.Add(this.textBoxLoginUsername);
             this.Controls.Add(this.labelSignup);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelSignupConfirmPass);
             this.Controls.Add(this.labelLoginPassword);
             this.Controls.Add(this.labelSignupPassword);
@@ -258,5 +328,11 @@
         private System.Windows.Forms.Label labelSigupEmail;
         private System.Windows.Forms.Label labelLoginUsername;
         private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.TextBox textBoxSignupFirstname;
+        private System.Windows.Forms.TextBox textBoxSignupLastname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker birthdatePickerSignup;
     }
 }
